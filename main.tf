c9134fe0705ddfd40bda6a56b8d70c5d4327263e
bblinder <filmmaven@gmail.com>
2022-09-15 09:09:26 -0400

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     signalfx = {
       source  = "splunk-terraform/signalfx"
-      version = "6.16.0"
+      version = "6.17.0"  # if the version is not included, Terraform will use the latest.
     }
   }
 }
@@ -13,33 +13,33 @@ provider "signalfx" {
   api_url    = "https://api.${var.signalfx_realm}.signalfx.com"
 }
 
-resource "signalfx_time_chart" "mychart0" {
-  name        = "CPU Utilization"
-  description = "Very cool chart"
+# resource "signalfx_time_chart" "mychart0" {
+#   name        = "CPU Utilization"
+#   description = "Very cool chart"
 
-  program_text = <<-EOF
-    data("cpu.utilization", filter=filter('host', '*')).publish(label='CPU Utilization')
-    EOF
-}
+#   program_text = <<-EOF
+#     data("cpu.utilization", filter=filter('host', '*')).publish(label='CPU Utilization')
+#     EOF
+# }
 
-resource "signalfx_dashboard_group" "mydashgroup0" {
-  name        = "Workshop Group - ${var.username}"
-  description = "${var.username}'s Awesome Workshop Group"
-}
+# resource "signalfx_dashboard_group" "mydashgroup0" {
+#   name        = "Workshop Group - ${var.username}"
+#   description = "${var.username}'s Awesome Workshop Group"
+# }
 
-resource "signalfx_dashboard" "mydashboard0" {
-  name            = "Workshop Dashboard - ${var.username}"
-  dashboard_group = signalfx_dashboard_group.mydashgroup0.id
+# resource "signalfx_dashboard" "mydashboard0" {
+#   name            = "Workshop Dashboard - ${var.username}"
+#   dashboard_group = signalfx_dashboard_group.mydashgroup0.id
 
-  chart {
-    chart_id = signalfx_time_chart.mychart0.id
-    width    = 12
-    height   = 1
-    row      = 0
-  }
-}
+#   chart {
+#     chart_id = signalfx_time_chart.mychart0.id
+#     width    = 12
+#     height   = 1
+#     row      = 0
+#   }
+# }
 
-# print URL to dashboard
-output "dashboard_group_URL" {
-  value = "https://app.${var.signalfx_realm}.signalfx.com/#/dashboard/${signalfx_dashboard.mydashboard0.id}"
-}
+# # print URL to dashboard
+# output "dashboard_group_URL" {
+#   value = "https://app.${var.signalfx_realm}.signalfx.com/#/dashboard/${signalfx_dashboard.mydashboard0.id}"
+# }
